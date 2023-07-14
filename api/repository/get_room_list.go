4ef5ffafb14cb625b71a4cd7039fec1456084bee
pollenjp <polleninjp@gmail.com>
2023-07-14 19:29:41 +0900

    Add GetRoomListFilteredByLiveId

--- api/repository/get_room_list.go
+++ api/repository/get_room_list.go
@@ -55,3 +55,56 @@ func (r *Repository) GetRoomList(
 	}
 	return roomList, nil
 }
+
+func (r *Repository) GetRoomListFilteredByLiveId(
+	ctx context.Context,
+	db service.Queryer,
+	RoomStatus entity.RoomStatus,
+	liveId entity.LiveId,
+) ([]*service.RoomInfoItem, error) {
+	roomList := []*service.RoomInfoItem{}
+
+	sql := `
+	WITH
+		-- live_id と room.status で絞り込んだ room テーブル
+		filtered_room AS (
+			SELECT
+				id,
+				live_id,
+				host_user_id,
+				status,
+				created_at
+			FROM
+				room
+			WHERE
+				live_id = ?
+				AND
+				status = ?
+		)
+	SELECT
+		filtered_room.id AS "room_id",
+		filtered_room.live_id AS "live_id",
+		COUNT(room_user.user_id) AS "joined_user_count"
+	FROM
+		room_user
+		INNER JOIN filtered_room
+			ON
+				room_user.room_id = filtered_room.id
+	GROUP BY
+		room_id
+	ORDER BY
+		filtered_room.created_at ASC
+	;`
+
+	err := db.SelectContext(
+		ctx,
+		&roomList,
+		sql,
+		liveId,
+		RoomStatus,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return roomList, nil
+}
